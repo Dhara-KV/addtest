b3304744201180f786399b98ac11d11c619d9ecd
Dhara-kishoree <125853153+Dhara-kishoree@users.noreply.github.com>
2023-03-10 12:42:59 +0530

    commit

---
+++ src/main/java/com/example/demo/Test5Application.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@SpringBootApplication
+public class Test5Application {
+	
+	@PostMapping
+	public String message(){
+		return "your app is deployed in azure sucessfully";
+	}
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(Test5Application.class, args);
+	}
+
+}
