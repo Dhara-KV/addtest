70e63cba28aa4986c0b4d8d1d92dd420874822df
Dhara-kishoree <125853153+Dhara-kishoree@users.noreply.github.com>
2023-03-13 10:41:54 +0530

    my deploy

--- src/main/java/com/example/demo/Test5Application.java
+++ src/main/java/com/example/demo/Test5Application.java
@@ -2,13 +2,14 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 @SpringBootApplication
 public class Test5Application {
 	
-	@PostMapping
+	@GetMapping
 	public String message(){
 		return "your app is deployed in azure sucessfully";
 	}
